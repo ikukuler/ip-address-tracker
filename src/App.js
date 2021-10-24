66d4881e4ed6af6134f8f49abba859e6b500db23
Igor Kukuler <ikukuler@gmail.com>
2021-10-25 02:16:21 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { getLocationByIP } from "./ip-api";
+import Searchbar from "./components/searchbar";
+import InfoWrapper from "./components/info-wrapper";
+import MapWrapper from "./components/map-wrapper/MapWrapper";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ErrorBoundary = ({ error, children }) => {
+	const errorObject = error && error.toJSON();
+	if (errorObject) {
+		return (
+			<div className="container skeleton">
+				<div className="error">
+					<h2>{ errorObject.status === 422 ? "Probaly wrong IP" : errorObject.message }</h2>
+				</div>
+			</div>
+		)
+	} 
+
+	return(
+		<> 
+			{ children }
+		</>
+	);
+}
+
+const App = () => {
+
+	const [isLoading, setIsLoading] = useState(true);
+	const [data, setData] = useState({});
+	const [error, setError] = useState(null);
+
+	const populateData = (ip) => {
+		setIsLoading(true);
+		setError(null);
+		getLocationByIP(ip)
+			.then(response => {
+				if (response.statusText === "OK") {
+					setData(response.data);
+					setIsLoading(false);
+				}
+			})
+			.catch((error) => {
+				console.log(error)
+				setError(error);
+				setIsLoading(false);
+			});
+	}
+
+	useEffect(() => {
+		populateData();
+	}, []);
+	
+	return ( 
+		<div>
+			<Searchbar populateData={populateData} />
+			<ErrorBoundary error={error}>
+				<InfoWrapper data={data} isLoading={isLoading} />
+				<MapWrapper data={data} isLoading={isLoading} />
+			</ErrorBoundary>
+		</div>
+	)
 }
 
 export default App;
